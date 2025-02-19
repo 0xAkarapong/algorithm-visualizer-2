1e65cb0afda28a15f01bd212460f500fb9e82b11
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-02-19 12:25:45 +0700

    feat: implement Next.js server with custom request handling and environment configuration

--- server.ts
+++ server.ts
@@ -1,3 +1,21 @@
-Deno.serve((_req) => {
-    return new Response("Hello, World!");
-  });
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+ 
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+ 
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+ 
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})
